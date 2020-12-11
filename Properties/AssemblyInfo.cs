51102dbb31b319f61b9c2f36f8b7e1afce6ef378
Matix-Media <support@matix-media.de>
2020-12-11 16:38:50 +0100

    Projektdateien hinzufügen.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Matix.Controls.Progressbar")]
+[assembly: AssemblyDescription("A simple flat looking ProgressBar with the option to change the Color.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Matix Media, Inc.")]
+[assembly: AssemblyProduct("Matix.Controls.Progressbar")]
+[assembly: AssemblyCopyright("Copyright © Matix Media, Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("508abc75-2360-43bf-bb23-a2e5a3194dfc")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.5.20346")]
+[assembly: AssemblyFileVersion("1.0.5.20346")]
